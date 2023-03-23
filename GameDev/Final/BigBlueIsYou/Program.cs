d24161488c2c6704d8222b7ae988bb949f016a35
Tyler Conley <tyler.conley@sdl.usu.edu>
2023-03-23 14:39:21 -0600

    added skeleton project for Final Game

---
+++ GameDev/Final/BigBlueIsYou/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CS5410
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Assignment())
+            {
+                game.Run();
+            }
+        }
+    }
+}
